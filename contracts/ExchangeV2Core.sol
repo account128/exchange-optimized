9ba01a288df6b0147401ed43be4437c683341b42
Sussy Kneel <sussykneelsnft@protonmail.com>
2022-04-05 23:58:37 -0400

    remove fill storage and cancel()

--- contracts/ExchangeV2Core.sol
+++ contracts/ExchangeV2Core.sol
@@ -22,21 +22,7 @@ abstract contract ExchangeV2Core is
   using SafeMathUpgradeable for uint256;
   using LibTransfer for address;
 
-  uint256 private constant UINT256_MAX = 2**256 - 1;
-
-  //state of the orders
-  mapping(bytes32 => uint256) public fills;
-
-  //events
-  event Cancel(
-    bytes32 hash,
-    address maker,
-    LibAsset.AssetType makeAssetType,
-    LibAsset.AssetType takeAssetType
-  );
   event Match(
-    bytes32 leftHash,
-    bytes32 rightHash,
     address leftMaker,
     address rightMaker,
     uint256 newLeftFill,
@@ -44,19 +30,6 @@ abstract contract ExchangeV2Core is
     LibAsset.AssetType leftAsset,
     LibAsset.AssetType rightAsset
   );
-
-  function cancel(LibOrder.Order memory order) external {
-    require(_msgSender() == order.maker, "not a maker");
-    require(order.salt != 0, "0 salt can't be used");
-    bytes32 orderKeyHash = LibOrder.hashKey(order);
-    fills[orderKeyHash] = UINT256_MAX;
-    emit Cancel(
-      orderKeyHash,
-      order.maker,
-      order.makeAsset.assetType,
-      order.takeAsset.assetType
-    );
-  }
 
   function matchOrders(
     LibOrder.Order memory orderLeft,
@@ -89,8 +62,6 @@ abstract contract ExchangeV2Core is
       LibAsset.AssetType memory makeMatch,
       LibAsset.AssetType memory takeMatch
     ) = matchAssets(orderLeft, orderRight);
-    bytes32 leftOrderKeyHash = LibOrder.hashKey(orderLeft);
-    bytes32 rightOrderKeyHash = LibOrder.hashKey(orderRight);
 
     LibOrderDataV2.DataV2 memory leftOrderData = LibOrderData.parse(orderLeft);
     LibOrderDataV2.DataV2 memory rightOrderData = LibOrderData.parse(
@@ -100,8 +71,6 @@ abstract contract ExchangeV2Core is
     LibFill.FillResult memory newFill = getFillSetNew(
       orderLeft,
       orderRight,
-      leftOrderKeyHash,
-      rightOrderKeyHash,
       leftOrderData,
       rightOrderData
     );
@@ -128,8 +97,6 @@ abstract contract ExchangeV2Core is
       }
     }
     emit Match(
-      leftOrderKeyHash,
-      rightOrderKeyHash,
       orderLeft.maker,
       orderRight.maker,
       newFill.rightValue,
@@ -142,13 +109,11 @@ abstract contract ExchangeV2Core is
   function getFillSetNew(
     LibOrder.Order memory orderLeft,
     LibOrder.Order memory orderRight,
-    bytes32 leftOrderKeyHash,
-    bytes32 rightOrderKeyHash,
     LibOrderDataV2.DataV2 memory leftOrderData,
     LibOrderDataV2.DataV2 memory rightOrderData
-  ) internal returns (LibFill.FillResult memory) {
-    uint256 leftOrderFill = getOrderFill(orderLeft, leftOrderKeyHash);
-    uint256 rightOrderFill = getOrderFill(orderRight, rightOrderKeyHash);
+  ) internal pure returns (LibFill.FillResult memory) {
+    uint256 leftOrderFill = 0; // 0 as order is unfilled
+    uint256 rightOrderFill = 0; // 0 as order is unfilled
     LibFill.FillResult memory newFill = LibFill.fillOrder(
       orderLeft,
       orderRight,
@@ -159,35 +124,7 @@ abstract contract ExchangeV2Core is
     );
 
     require(newFill.rightValue > 0 && newFill.leftValue > 0, "nothing to fill");
-
-    if (orderLeft.salt != 0) {
-      if (leftOrderData.isMakeFill) {
-        fills[leftOrderKeyHash] = leftOrderFill.add(newFill.leftValue);
-      } else {
-        fills[leftOrderKeyHash] = leftOrderFill.add(newFill.rightValue);
-      }
-    }
-
-    if (orderRight.salt != 0) {
-      if (rightOrderData.isMakeFill) {
-        fills[rightOrderKeyHash] = rightOrderFill.add(newFill.rightValue);
-      } else {
-        fills[rightOrderKeyHash] = rightOrderFill.add(newFill.leftValue);
-      }
-    }
     return newFill;
-  }
-
-  function getOrderFill(LibOrder.Order memory order, bytes32 hash)
-    internal
-    view
-    returns (uint256 fill)
-  {
-    if (order.salt == 0) {
-      fill = 0;
-    } else {
-      fill = fills[hash];
-    }
   }
 
   function matchAssets(
